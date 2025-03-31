d7908f064e36d47bba22266ccea1c3fad3107c38
fulmicotone98 <jacopo.catalano98@gmail.com>
2025-03-31 22:07:25 +0200

    fix: fix on main()

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-use zero2prod::run;
+use std::net::TcpListener;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    run()?.await
+async fn main() {
+    // bind port 0 will trigger an OS scan for an available port which will then be bound to the application
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    // let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+
+    // Launch server as a background task thanks to tokio::spawn
+    tokio::spawn(server);
+
+    // format!("http://127.0.0.1:{}", port)
 }
