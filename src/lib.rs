dadd4fa2248819ab7ac20925cd37cede1baeda86
fulmicotone98 <jacopo.catalano98@gmail.com>
2025-04-09 22:29:29 +0200

    feature: /subscription route added with early tests

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,11 @@ use actix_web::{
 };
 
 async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
@@ -17,7 +21,12 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         // After HttpServer has established a new connection with a client,
         // App start handling all the request to the APIs.
-        App::new().route("/health_check", web::get().to(health_check))
+        App::new()
+
+            .route("/health_check", web::get().to(health_check))
+
+            // A new entry in our routing table for POST /subscriptions requests
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
